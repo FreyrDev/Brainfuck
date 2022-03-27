d1b4f437bb75f63b2a236a181362f969698a9696
Freyr <95385132+FreyrDev@users.noreply.github.com>
2022-03-27 22:30:57 +0100

    Added loop functionality
    [ and ] now work as BF operations

--- interpreter.nim
+++ interpreter.nim
@@ -7,6 +7,24 @@ var
   codePtr = 0
 
 
+proc matchBrackets(input: string, index: int, shape: char): int =
+  var sequence: seq[char]
+
+  template findIndex(open, close, iter): untyped =
+    for j in iter:
+      if input[j] == open:
+        sequence.delete(0)
+      elif input[j] == close:
+        sequence.add(input[index])
+      if sequence == @[]:
+        return j
+
+  if shape == '[':
+    findIndex('[', ']', index..<input.len)
+  elif shape == ']':
+    findIndex(']', '[', index..\0)
+
+
 proc interpret(code: string) =
   while dataPtr >= 0 and codePtr < code.len():
     case code[codePtr]:
@@ -16,8 +34,12 @@ proc interpret(code: string) =
       of '-': data[dataPtr].dec()
       of '.': data[dataPtr].char.write()
       of ',': data[dataPtr] = readChar().byte
+      of '[':
+        if data[dataPtr] == 0: codePtr = code.matchBrackets(codePtr, '[')
+      of ']':
+        if data[dataPtr] != 0: codePtr = code.matchBrackets(codePtr, ']')
       else: discard
     codePtr.inc()
 
-let input = readLine()
+let input = readFile("helloworld.txt")
 interpret(input)
